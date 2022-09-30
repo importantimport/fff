a27d46563dc468f47c6ea3dde75911326d3c967e
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-09-30 14:55:12 +0800

    chore(docs): 📈 add plausible

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -60,10 +60,39 @@ export default defineConfig({
   },
   head: [
     ['link', { rel: 'icon', href: '/glowing_star.svg', type: 'image/svg+xml' }],
-    ['link', { rel: 'manifest', href: '/manifest.webmanifest', type: 'application/manifest+json'}],
-    ['meta', { property: 'og:image', content: 'https://og-image.vercel.app/**FFF**%20Flavored%20Frontmatter.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fvercel-triangle-white.svg&images=https%3A%2F%2Ffff.js.org%2Fglowing_star.svg&widths=256&widths=384&heights=256&heights=384'}],
-    ['meta', { property: 'twitter:image', content: 'https://og-image.vercel.app/**FFF**%20Flavored%20Frontmatter.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fvercel-triangle-white.svg&images=https%3A%2F%2Ffff.js.org%2Fglowing_star.svg&widths=256&widths=384&heights=256&heights=384'}],
-    ['meta', { property: 'twitter:card', content: 'summary'}]
+    [
+      'link',
+      {
+        rel: 'manifest',
+        href: '/manifest.webmanifest',
+        type: 'application/manifest+json',
+      },
+    ],
+    [
+      'meta',
+      {
+        property: 'og:image',
+        content:
+          'https://og-image.vercel.app/**FFF**%20Flavored%20Frontmatter.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fvercel-triangle-white.svg&images=https%3A%2F%2Ffff.js.org%2Fglowing_star.svg&widths=256&widths=384&heights=256&heights=384',
+      },
+    ],
+    [
+      'meta',
+      {
+        property: 'twitter:image',
+        content:
+          'https://og-image.vercel.app/**FFF**%20Flavored%20Frontmatter.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fvercel-triangle-white.svg&images=https%3A%2F%2Ffff.js.org%2Fglowing_star.svg&widths=256&widths=384&heights=256&heights=384',
+      },
+    ],
+    ['meta', { property: 'twitter:card', content: 'summary' }],
+    [
+      'script',
+      {
+        src: 'https://plausible.kwaa.dev/js/plausible.js',
+        defer: '',
+        'data-domain': 'fff.js.org',
+      },
+    ],
   ],
   themeConfig: {
     nav: nav(),
