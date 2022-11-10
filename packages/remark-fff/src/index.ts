ee5a3234a7bddd722042d5270e77909dd7105501
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-11-10 13:15:38 +0800

    refactor(remark-fff): 🏷️ improve types

--- packages/remark-fff/src/index.ts
+++ packages/remark-fff/src/index.ts
@@ -1,5 +1,6 @@
 import type { FFFFlavoredFrontmatter } from 'fff-flavored-frontmatter'
-import type { Plugin, Transformer } from 'unified'
+import type { Processor, TransformCallback } from 'unified'
+import type { Node } from 'unist'
 import * as presets from './presets'
 
 export type RemarkFFFOptions = {
@@ -17,28 +18,63 @@ export type RemarkFFFPreset = {
   [key in keyof FFFFlavoredFrontmatter]: string | ((fm: Frontmatter) => unknown)
 }
 
+type Transformer<Input extends Node = Node, Output extends Node = Input> = (
+  node: Input,
+  file: File,
+  next: TransformCallback<Output>
+) => Promise<Output | undefined | void> | Output | Error | undefined | void
+
+type Plugin<
+  PluginParameters extends any[] = any[],
+  Input = Node,
+  Output = Input
+> = (
+  this: Input extends Node
+    ? Output extends Node
+      ? // This is a transform, so define `Input` as the current tree.
+        Processor<void, Input>
+      : // Compiler.
+        Processor<void, Input, Input, Output>
+    : Output extends Node
+    ? // Parser.
+      Processor<Output, Output>
+    : // No clue.
+      Processor,
+  ...settings: PluginParameters
+) => // If both `Input` and `Output` are `Node`, expect an optional `Transformer`.
+Input extends Node
+  ? Output extends Node
+    ? Transformer<Input, Output> | void
+    : void
+  : void
+
 type Frontmatter = FFFFlavoredFrontmatter & {
   [key: string]: unknown
 }
 
 type File = {
-  data: {
-    fm?: Frontmatter
-    astro?: {
-      frontmatter?: Frontmatter
-    }
-  }
+  data:
+    | {
+        /** MDsveX */
+        fm: Frontmatter
+        astro: never
+      }
+    | {
+        fm: never
+        /** Astro */
+        astro: {
+          frontmatter: Frontmatter
+        }
+      }
 }
 
-const remarkFFF: Plugin =
-  (
-    options: RemarkFFFOptions = { presets: ['hugo'], target: 'mdsvex' }
-  ): Transformer =>
-  (_tree, file: File) => {
+const remarkFFF: Plugin<[RemarkFFFOptions]> =
+  (options = { presets: ['hugo'], target: 'mdsvex' }) =>
+  (_tree, file) => {
     let fm = {
       ...(options.target === 'mdsvex'
         ? file.data.fm
-        : file.data.astro!.frontmatter),
+        : file.data.astro.frontmatter),
     }
     ;[
       ...options.presets,
