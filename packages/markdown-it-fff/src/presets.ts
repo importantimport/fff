29629760aa4df65bd73ae496e20054cf712afc59
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-11-21 17:48:07 +0800

    fix(markdown-it-fff): 🐛 export directly

--- packages/markdown-it-fff/src/presets.ts
+++ packages/markdown-it-fff/src/presets.ts
@@ -1,15 +1,12 @@
-import * as presets from '../../remark-fff/src/presets'
 import type { MarkdownItFFFPreset } from './index'
+
+/** Remark FFF Presets */
+export * from '../../remark-fff/src/presets'
 
 /**
  * @alpha
  * @see {@link https://github.com/mdit-vue/mdit-vue/tree/main/packages/plugin-frontmatter}
  */
-const vue: MarkdownItFFFPreset = {
+export const vue: MarkdownItFFFPreset = {
   summary: 'excerpt',
-}
-
-export default {
-  ...presets,
-  vue,
 }
