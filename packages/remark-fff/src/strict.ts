157c2e925e2839492e8c2c62fb7a861a543c576c
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-11-24 16:07:19 +0800

    refactor(remark-fff): 🎨 improve strict preset

--- packages/remark-fff/src/strict.ts
+++ packages/remark-fff/src/strict.ts
@@ -1,39 +1,24 @@
-import type {
-  FFFAudio,
-  FFFFlavoredFrontmatter,
-  FFFImage,
-  FFFVideo,
-} from 'fff-flavored-frontmatter'
+import type { FFFAudio, FFFImage, FFFVideo } from 'fff-flavored-frontmatter'
 import type { RemarkFFFPreset, RemarkFFFOptions } from './index'
 
+const mediaTransform = (
+  options: RemarkFFFOptions['strict']['media'],
+  media: string | (FFFImage | FFFAudio | FFFVideo)
+) =>
+  typeof media === 'string'
+    ? options.type === 'object'
+      ? { src: media }
+      : media
+    : options.type === 'string'
+    ? media.src
+    : media
+
 /** @alpha */
-export const strict = (strict: RemarkFFFOptions['strict']): RemarkFFFPreset => {
-  const mediaTransform = (
-    media:
-      | FFFFlavoredFrontmatter['image']
-      | FFFFlavoredFrontmatter['audio']
-      | FFFFlavoredFrontmatter['video']
-  ) => {
-    let result = media
-    if (result instanceof String && strict.media.type === 'object')
-      result = { src: result } as FFFImage | FFFAudio | FFFVideo
-    else if (result instanceof Object && strict.media.type === 'string')
-      result = (result as FFFImage | FFFAudio | FFFVideo).src
-    else if (result instanceof Array) {
-      result = result.map((media: string | FFFImage) =>
-        media instanceof String && strict.media.type === 'object'
-          ? ({ src: result } as FFFImage | FFFAudio | FFFVideo)
-          : media instanceof Object && strict.media.type === 'string'
-          ? (media as FFFImage | FFFAudio | FFFVideo).src
-          : media
-      )
-      if (strict.media.array === false) result = result[0]
-    }
-    return result
-  }
-  return {
-    image: ({ image }) => mediaTransform(image),
-    audio: ({ audio }) => mediaTransform(audio),
-    video: ({ video }) => mediaTransform(video),
-  }
-}
+export const strict = (
+  strict: RemarkFFFOptions['strict']
+): RemarkFFFPreset => ({
+  images: ({ images }) =>
+    images.map((image: string | FFFImage) => mediaTransform(strict.media, image)),
+  audio: ({ audio }) => mediaTransform(strict.media, audio),
+  video: ({ video }) => mediaTransform(strict.media, video),
+})
