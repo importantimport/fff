e95eb80214c0bb72c17979cc5b91be05555514df
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-01-01 20:20:08 +0800

    refactor(remark-fff): 🏷️ improve types
    ➖ remove @types/unist

--- packages/remark-fff/src/index.ts
+++ packages/remark-fff/src/index.ts
@@ -1,6 +1,5 @@
 import type { FFFFlavoredFrontmatter } from 'fff-flavored-frontmatter'
-import type { Processor, TransformCallback } from 'unified'
-import type { Node } from 'unist'
+import type { Plugin, Transformer } from 'unified'
 import * as presets from './presets'
 import * as autofill from './autofill'
 import { strict } from './strict'
@@ -26,41 +25,11 @@ export type RemarkFFFOptions = {
   }
 }
 
-type Transformer<Input extends Node = Node, Output extends Node = Input> = (
-  node: Input,
-  file: File,
-  next: TransformCallback<Output>
-) => Promise<Output | undefined | void> | Output | Error | undefined | void
-
-type Plugin<
-  PluginParameters extends any[] = any[],
-  Input = Node,
-  Output = Input
-> = (
-  this: Input extends Node
-    ? Output extends Node
-      ? // This is a transform, so define `Input` as the current tree.
-        Processor<void, Input>
-      : // Compiler.
-        Processor<void, Input, Input, Output>
-    : Output extends Node
-    ? // Parser.
-      Processor<Output, Output>
-    : // No clue.
-      Processor,
-  ...settings: PluginParameters
-) => // If both `Input` and `Output` are `Node`, expect an optional `Transformer`.
-Input extends Node
-  ? Output extends Node
-    ? Transformer<Input, Output> | void
-    : void
-  : void
-
 type Frontmatter = FFFFlavoredFrontmatter & {
   [key: string]: unknown
 }
 
-type File =
+type Post =
   | /** MDsveX */ {
       filename: string
       path: never
@@ -81,14 +50,15 @@ type File =
     }
 
 const remarkFFF: Plugin<[RemarkFFFOptions]> =
-  (options = { presets: ['hugo', 'legacy'], target: 'mdsvex' }) =>
+  (options = { presets: ['hugo', 'legacy'], target: 'mdsvex' }): Transformer =>
   (_tree, file) => {
-    const path = file.filename ?? file.path
+    let post: Post = file as unknown as Post
     let fm = {
       ...(options.target === 'mdsvex'
-        ? file.data.fm
-        : file.data.astro.frontmatter),
+        ? post.data.fm
+        : post.data.astro.frontmatter),
     }
+    const path = post.filename ?? post.path
     ;[
       ...options.presets,
       ...(options.autofill?.provider
@@ -98,7 +68,7 @@ const remarkFFF: Plugin<[RemarkFFFOptions]> =
                 ? options.autofill.path instanceof Function
                   ? options.autofill.path(path)
                   : autofill.path[options.autofill.path](path)
-                : file.filename
+                : post.filename
             ),
           ]
         : []),
@@ -116,7 +86,11 @@ const remarkFFF: Plugin<[RemarkFFFOptions]> =
       )
     )
     if (options.target === 'mdsvex') file.data.fm = fm
-    else if (options.target === 'astro') file.data.astro!.frontmatter = fm
+    else if (options.target === 'astro')
+      file.data.astro = {
+        ...(file.data.astro as Object),
+        frontmatter: fm,
+      }
   }
 
 export default remarkFFF
