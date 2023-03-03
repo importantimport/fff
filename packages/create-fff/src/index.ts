8132809afc2179198bb856323e4ea34d4efaf24a
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-03-03 23:28:05 +0800

    feat: ✨ init `create-fff`
    ➕ add @clack/prompts, picocolors, yaml

---
+++ packages/create-fff/src/index.ts
@@ -0,0 +1,48 @@
+import {
+  intro,
+  outro,
+  select,
+  isCancel,
+  cancel,
+} from '@clack/prompts'
+import color from 'picocolors'
+
+import { netlifyCMS } from './lib/netlify-cms'
+
+const main = async () => {
+  console.clear()
+  
+  intro(color.black(color.bgCyan(' create-fff ')))
+
+  const type = await select({
+    message: 'Choose what you need to create:',
+    options: [
+      {
+        value: 'netlify-cms',
+        label: 'Netlify CMS Config (WIP)',
+        hint: 'config.yml',
+      },
+      {
+        value: 'contentlayer',
+        label: 'Contentlayer Schema (TODO)',
+        hint: 'contentlayer.config.ts',
+      },
+    ],
+  })
+
+  if (isCancel(type)) {
+    cancel('Operation cancelled')
+    return process.exit(0)
+  }
+
+  switch (type) {
+    case 'netlify-cms':
+      await netlifyCMS()
+      process.exit(0)
+    default:
+      outro(type as string)
+      process.exit(0)
+  }
+}
+
+main().catch(console.error)
---
+++ packages/create-fff/src/lib/netlify-cms.ts
@@ -0,0 +1,59 @@
+import {
+  outro,
+  confirm,
+  isCancel,
+  cancel,
+  text,
+  note,
+} from '@clack/prompts'
+import color from 'picocolors'
+import fs from 'node:fs/promises'
+import { exec } from 'node:child_process'
+import { promisify } from 'node:util'
+import { stringify } from 'yaml'
+import { version } from '../../package.json'
+
+export const netlifyCMS = async () => {
+  const path = await text({
+    message: 'WWhere should we create your Netlify CMS config?',
+    placeholder: './public/admin/config.yml',
+    validate: (value) => {
+      if (!value) return 'Please enter a path.'
+      if (value[0] !== '.') return 'Please enter a relative path.'
+    },
+  })
+
+  if (isCancel(path)) {
+    cancel('Operation cancelled')
+    return process.exit(0)
+  }
+
+  await fs
+    .access(path, fs.constants.F_OK)
+    .then(async () => {
+      const check = await confirm({
+        message: color.yellow(
+          `${path} already has files. Do you want to continue?`
+        ),
+      })
+      if (isCancel(check) || check === false) {
+        cancel('Operation cancelled')
+        return process.exit(0)
+      }
+    })
+    .catch(() => {})
+
+  await fs.writeFile(
+    path,
+    stringify({
+      backend: {
+        name: 'git-gateway',
+        branch:
+          (await promisify(exec)('git branch --show-current')).stdout.trim() ?? 'main',
+      },
+    })
+  )
+
+  note(`FFF Flavored Frontmatter\nVersion ${version}`)
+  outro("You're all set! ")
+}
