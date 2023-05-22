ccffdb86136e469f2c5cef79b115bb9d2c0ba9db
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-05-22 12:08:22 +0800

    refactor(fff-flavored-frontmatter): 🚚 rename folder

--- packages/fff-flavored-frontmatter/fff.ts
+++ packages/fff-flavored-frontmatter/fff.ts
@@ -1,4 +1,2 @@
-// deno-fmt-ignore-file fuck the semicolons
-
-export * from './lib/types'
-export * from './lib/utils'
+export * from './src/types'
+export * from './src/utils'
--- packages/fff-flavored-frontmatter/src/types.ts
+++ packages/fff-flavored-frontmatter/src/types.ts
@@ -1,5 +1,4 @@
 /// <reference lib="dom" />
-// deno-fmt-ignore-file fuck the semicolons
 
 /** Object Image */
 export type FFFImage = Partial<HTMLImageElement> & {
