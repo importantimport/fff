0d9a5415ad34fd2ebeb4c3d5ff76f829af2d8ac0
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-05-24 00:03:28 +0800

    refactor(presets): ♻️ add mdit-vue preset

--- packages/fff-transform-presets/src/index.ts
+++ packages/fff-transform-presets/src/index.ts
@@ -1,4 +1,8 @@
+// common
 export * from './common/hexo'
 export * from './common/hugo'
 export * from './common/jekyll'
 export * from './common/nuxt'
+
+// markdown-it
+export * from './mdit/vue'
---
+++ packages/fff-transform-presets/src/mdit/vue.ts
@@ -0,0 +1,19 @@
+import type { FFFTransformPreset, FFFTransformPresetReverse } from 'fff-flavored-frontmatter'
+
+/**
+ * `@mdit-vue/plugin-frontmatter` - FFF Transform Preset
+ * @beta
+ * @see {@link https://github.com/mdit-vue/mdit-vue/tree/main/packages/plugin-frontmatter}
+ */
+export const mditVue: FFFTransformPreset = {
+  summary: 'excerpt',
+}
+
+/**
+ * `@mdit-vue/plugin-frontmatter` - FFF Transform Preset (Reverse)
+ * @beta
+ * @see {@link https://github.com/mdit-vue/mdit-vue/tree/main/packages/plugin-frontmatter}
+ */
+export const mditVueReverse: FFFTransformPresetReverse = {
+  excerpt: 'summary',
+}
