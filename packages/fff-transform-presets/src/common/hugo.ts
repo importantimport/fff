9396b473c40e5ef7d9c68fab444298362160dd15
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-05-23 23:53:09 +0800

    refactor(fff-transform-presets): ♻️ update presets

--- packages/fff-transform-presets/src/common/hugo.ts
+++ packages/fff-transform-presets/src/common/hugo.ts
@@ -1,34 +1,44 @@
-import type { FFFFlavoredFrontmatter, FFFTransformPreset } from 'fff-flavored-frontmatter'
+import type { FFFFlavoredFrontmatter, FFFTransformPreset, FFFTransformPresetReverse } from 'fff-flavored-frontmatter'
 
 /**
  * Hugo - FFF Transform Preset
  * @beta
  * @see {@link https://gohugo.io/content-management/front-matter/#predefined}
- * @see {@link https://github.com/getindiekit/indiekit/blob/main/packages/preset-hugo/index.js}
  */
 export const hugo: FFFTransformPreset = {
-  bookmark_of: 'bookmarkOf',
+  // TODO: images, videos
   flags: ({
     flags,
     draft,
-    visibility,
   }: {
     flags?: FFFFlavoredFrontmatter['flags']
     draft?: boolean
-    visibility?: 'public' | 'unlisted' | 'private'
   }) =>
     [...new Set([
       ...(flags ?? []),
       ...(draft ? ['draft'] : []),
-      ...(visibility ? [visibility] : []),
     ])],
-  image: ({ image, images }) =>
-    image || ((images && (!Array.isArray(images) || images.length === 1))
-      ? images
-      : undefined),
-  images: ({ images }) => (Array.isArray(images) ? images : undefined),
-  in_reply_to: 'inReplyTo',
-  like_of: 'likeOf',
-  repost_of: 'repostOf',
-  tags: 'category',
+  published: 'publishDate',
+  tags: ({ tags, keywords }: {
+    tags?: FFFFlavoredFrontmatter['tags'],
+    keywords?: string[]
+  }) =>
+    [...new Set([
+      ...(tags ?? []),
+      ...(keywords ?? []),
+    ])],
+  updated: 'lastmod',
+}
+
+/**
+ * Hugo - FFF Transform Preset (Reverse)
+ * @alpha
+ * @see {@link https://gohugo.io/content-management/front-matter/#predefined}
+ */
+export const hugoReverse: FFFTransformPresetReverse = {
+  // TODO: images, videos
+  draft: ({ flags }) => flags?.includes('draft'),
+  keywords: 'tags',
+  lastmod: 'updated',
+  publishDate: 'published',
 }
--- packages/fff-transform-presets/src/common/jekyll.ts
+++ packages/fff-transform-presets/src/common/jekyll.ts
@@ -1,42 +1,33 @@
-import type { FFFFlavoredFrontmatter, FFFTransformPreset } from 'fff-flavored-frontmatter'
+import type { FFFTransformPreset, FFFTransformPresetReverse } from 'fff-flavored-frontmatter'
 
 /**
  * Jekyll - FFF Transform Preset
  * @beta
  * @see {@link https://jekyllrb.com/docs/front-matter/}
- * @see {@link https://github.com/getindiekit/indiekit/blob/main/packages/preset-jekyll/index.js}
  */
 export const jekyll: FFFTransformPreset = {
-  bookmark_of: 'bookmark-of',
-  created: 'date',
-  flags: ({
-    flags,
-    draft,
-    visibility,
-  }: {
-    flags?: FFFFlavoredFrontmatter['flags']
-    draft?: boolean
-    visibility?: 'public' | 'unlisted' | 'private'
-  }) =>
-    [...new Set([
-      ...(flags ?? []),
-      ...(draft ? ['draft'] : []),
-      ...(visibility ? [visibility] : []),
-    ])],
-  image: ({ photo }) => (Array.isArray(photo) ? undefined : photo),
-  images: ({ photo }) => (Array.isArray(photo) ? photo : undefined),
-  in_reply_to: 'in-reply-to',
-  like_of: 'like-of',
-  repost_of: 'repost-of',
-  summary: 'excerpt',
-  tags: ({
-    tags,
+  categories: ({
     category,
     categories,
   }: {
-    tags?: FFFFlavoredFrontmatter['tags']
-    category?: string[]
+    category?: string
     categories?: string[]
   }) =>
-    [...new Set([...(tags ?? []), ...(category ?? []), ...(categories ?? [])])],
+    [...new Set([category, ...(categories ?? [])])]
+      .filter(tag => tag !== undefined),
+  created: 'date',
+  tags: ({ tags }: {
+    tags?: string | string[]
+  }) => typeof tags === 'string'
+    ? tags.split(' ')
+    : tags,
+}
+
+/**
+ * Jekyll - FFF Transform Preset (Reverse)
+ * @alpha
+ * @see {@link https://jekyllrb.com/docs/front-matter/}
+ */
+export const jekyllReverse: FFFTransformPresetReverse = {
+  date: 'created',
 }
