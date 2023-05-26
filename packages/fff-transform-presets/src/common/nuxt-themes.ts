9ec4e490e8160f8cd97d11dc14878c7b0ddfadb0
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-05-26 20:19:33 +0800

    feat(presets): ✨ add alpine preset

---
+++ packages/fff-transform-presets/src/common/nuxt-themes.ts
@@ -0,0 +1,41 @@
+import type { FFFFlavoredFrontmatter, FFFTransformPreset, FFFTransformPresetReverse } from 'fff-flavored-frontmatter'
+
+type AlpineAuthor = {
+  name?: string
+  avatarUrl?: string
+  link?: string
+}
+
+/**
+ * Alpine - FFF Transform Preset
+ * @beta
+ * @see {@link https://github.com/nuxt-themes/alpine/blob/main/.starters/default/content/articles/1.get-started.md?plain=1}
+ */
+export const nuxtThemeAlpine: FFFTransformPreset = {
+  authors: ({ author }: FFFFlavoredFrontmatter & { author?: AlpineAuthor }) => author
+    ? [{
+      avatar: author.avatarUrl,
+      name: author.name,
+      url: author.link,
+    }]
+    : undefined,
+  created: 'date',
+  image: 'cover',
+}
+
+/**
+ * Alpine - FFF Transform Preset (Reverse)
+ * @alpha
+ * @see {@link https://github.com/nuxt-themes/alpine/blob/main/.starters/default/content/articles/1.get-started.md?plain=1}
+ */
+export const nuxtThemeAlpineReverse: FFFTransformPresetReverse = {
+  author: ({ authors }) => authors
+    ? {
+      avatarUrl: authors[0].avatar,
+      link: authors[0].url,
+      name: authors[0].name,
+    }
+    : undefined,
+  cover: 'image',
+  date: 'created',
+}
