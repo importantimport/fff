8b5904af989db0fe735b6ce1d1eba84b2d20e8dc
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-05-27 13:37:17 +0800

    fix(presets): 🐛 export alpine preset

--- packages/fff-transform-presets/src/index.ts
+++ packages/fff-transform-presets/src/index.ts
@@ -3,6 +3,7 @@ export * from './common/hexo'
 export * from './common/hugo'
 export * from './common/jekyll'
 export * from './common/nuxt'
+export * from './common/nuxt-themes'
 
 // markdown-it
 export * from './mdit/vue'
