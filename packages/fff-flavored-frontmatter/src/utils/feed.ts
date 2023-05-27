5e0b6bad1489753aa57ac5869455ffbfddba8fc8
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-05-27 20:17:50 +0800

    refactor(fff/ptd): ♻️ support event type

--- packages/fff-flavored-frontmatter/src/utils/feed.ts
+++ packages/fff-flavored-frontmatter/src/utils/feed.ts
@@ -5,30 +5,35 @@ import { transform } from './transform'
 /**
  * Supported Post Type
  * @beta
+ * @see {@link https://ptd.spec.indieweb.org/#algorithm}
+ * @see {@link https://indieweb.org/posts#Types_of_Posts}
  */
-export type postType = 'rsvp' | 'bookmark' | 'repost' | 'like' | 'reply' | 'video' | 'audio' | 'article' | 'photo' | 'note'
+export type postType = 'event' | 'rsvp' | 'repost' | 'like' | 'bookmark' | 'reply' | 'video' | 'audio' | 'article' | 'photo' | 'note'
 
 /**
  * Post Type Discovery
  * @beta
  * @param fm - FFF Flavored Frontmatter
  * @returns lower case post type
- * @see {@link https://ptd.spec.indieweb.org/}
+ * @see {@link https://ptd.spec.indieweb.org/#algorithm}
  * @see {@link https://indieweb.org/posts#Types_of_Posts}
  */
 export const postTypeDiscovery = (fm: FFFFlavoredFrontmatter): postType => {
-  // TODO: Event, Checkin, Reply with RSVP
-  if (
+  // TODO: Checkin, Reply with RSVP
+  /** may be modified in the future */
+  if (fm.flags?.includes('event'))
+    return 'event'
+  else if (
     fm.rsvp
     && ['yes', 'no', 'maybe', 'interested'].includes(fm.rsvp.toLowerCase())
   )
     return 'rsvp'
-  else if (fm.bookmark_of && new URL(fm.bookmark_of))
-    return 'bookmark'
   else if (fm.repost_of && new URL(fm.repost_of))
     return 'repost'
   else if (fm.like_of && new URL(fm.like_of))
     return 'like'
+  else if (fm.bookmark_of && new URL(fm.bookmark_of))
+    return 'bookmark'
   else if (
     fm.in_reply_to
     && new URL(Array.isArray(fm.in_reply_to) ? fm.in_reply_to[0] : fm.in_reply_to)
