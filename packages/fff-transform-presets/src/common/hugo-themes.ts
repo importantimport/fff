e7098694a5cdb2160ff2afc303b965e0f92a6977
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-06-03 23:00:13 +0800

    feat(presets): ✨ add papermod preset

---
+++ packages/fff-transform-presets/src/common/hugo-themes.ts
@@ -0,0 +1,35 @@
+import type { FFFTransformPreset } from 'fff-flavored-frontmatter'
+
+// eslint-disable-next-line unicorn/prevent-abbreviations
+type HugoThemePaperMod = {
+  /** To show multiple Authors */
+  author?: string | string[]
+  /** To set site description */
+  description?: string
+  /** Link or path of image for opengraph, twitter-cards */
+  images?: string
+}
+
+/**
+ * Hugo Theme PaperMod - FFF Transform Preset
+ * @alpha
+ * @see {@link https://adityatelange.github.io/hugo-PaperMod/posts/papermod/papermod-variables/}
+ */
+// eslint-disable-next-line unicorn/prevent-abbreviations
+export const hugoThemePaperMod: FFFTransformPreset<HugoThemePaperMod> = {
+  authors: ({ author }) => Array.isArray(author) ? author.map(name => ({ name })) : [{ name: author }],
+  image: 'images',
+  summary: 'description',
+}
+
+/**
+ * Hugo Theme PaperMod - FFF Transform Preset (Reverse)
+ * @alpha
+ * @see {@link https://adityatelange.github.io/hugo-PaperMod/posts/papermod/papermod-variables/}
+ */
+// eslint-disable-next-line unicorn/prevent-abbreviations
+export const hugoThemePaperModReverse: FFFTransformPreset<HugoThemePaperMod> = {
+  author: ({ authors }) => authors?.map(({ name }) => name),
+  description: 'summary',
+  images: 'image',
+}
