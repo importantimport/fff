e074f849cc2c2b9c2b7c1a6749d3e1ad0a854788
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-11-25 21:25:31 +0800

    chore: ⬆️ upgrade dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,17 @@
 // export { default } from '@importantimport/eslint-config'
-import ii from '@importantimport/eslint-config'
+import { createFullConfig } from '@importantimport/eslint-config'
 
-export default [
-  ...ii,
-  {
-    files: ['**/*.ts'],
-    languageOptions: {
-      parserOptions: {
-        project: ['./tsconfig.json', './tsconfig.node.json'],
-      },
-    },
-  },
-]
+// import ii from '@importantimport/eslint-config'
+
+// export default [
+//   ...ii,
+//   {
+//     files: ['**/*.ts'],
+//     languageOptions: {
+//       parserOptions: {
+//         project: ['./tsconfig.json', './tsconfig.node.json'],
+//       },
+//     },
+//   },
+// ]
+export default await createFullConfig({ ts: true })
