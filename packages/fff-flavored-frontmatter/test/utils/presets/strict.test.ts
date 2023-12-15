784dd0285ae0f2e62a4205beae83e62b2dfaa8a9
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-12-15 16:20:30 +0800

    chore(fff): ✅ add unit test

---
+++ packages/fff-flavored-frontmatter/test/utils/presets/strict.test.ts
@@ -0,0 +1,38 @@
+import { describe, expect, it } from 'vitest'
+
+import { type FFFFlavoredFrontmatter, strict, transform } from '../../../src'
+
+describe('fff-flavored-frontmatter strict preset', () => {
+  it('base', () => {
+    const input: FFFFlavoredFrontmatter = {
+      created: '2023-12-12',
+      title: 'Hello World!',
+    }
+    const output = transform(input, [
+      strict({
+        categories: false,
+        media: {
+          array: false,
+          type: 'string',
+        },
+      }),
+    ])
+
+    expect(output.created).toEqual('2023-12-12')
+  })
+
+  it('categories', () => {
+    const input: FFFFlavoredFrontmatter = {
+      categories: ['baz', 'qux'],
+      created: '2023-12-12',
+      tags: ['foo', 'bar'],
+      title: 'Categories Test',
+    }
+    const output = transform(input, [
+      strict({ categories: false }),
+    ])
+
+    // expect(output.categories).toBeUndefined()
+    expect(output.tags).toEqual(['foo', 'bar', 'baz', 'qux'])
+  })
+})
