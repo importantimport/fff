99d234e690769d5c7dc93802167464db36272446
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-12-20 19:10:18 +0800

    feat(fff/strict): ✨ draft & visibility

--- packages/fff-flavored-frontmatter/src/utils/presets/strict.ts
+++ packages/fff-flavored-frontmatter/src/utils/presets/strict.ts
@@ -7,17 +7,33 @@ import type { FFFTransformPreset } from '../transform.ts'
  */
 export interface StrictPresetOptions {
   /**
-   * transform categories and tags.
+   * transform {@link https://fff.js.org/version/1.2.html#categories | categories} and {@link https://fff.js.org/version/1.2.html#tags | tags}.
    * @defaultValue `undefined`
    * - `true` - merge tags into categories (tags unchanged)
    * - `false` - merge categories into tags (categories unchanged)
    * - `undefined` - do not transform tags and categories
    */
   categories?: boolean
+  /**
+   * transform {@link https://fff.js.org/version/1.2.html#draft | draft} and {@link https://fff.js.org/concepts/flags.html#post-status | draft flags}.
+   * @defaultValue `undefined`
+   * - `true` - merge draft flags into draft (flags unchanged)
+   * - `false` - merge draft into draft flags (draft unchanged)
+   * - `undefined` - do not transform draft and draft flags
+   */
+  draft?: boolean
   media?: {
     array?: boolean
     type?: 'object' | 'string'
   }
+  /**
+   * transform {@link https://fff.js.org/version/1.2.html#visibility | visibility} and {@link https://fff.js.org/concepts/flags.html#visibility | visibility flags}.
+   * @defaultValue `undefined`
+   * - `true` - merge visibility flags into visibility (flags unchanged)
+   * - `false` - merge visibility into visibility flags (visibility unchanged)
+   * - `undefined` - do not transform visibility and visibility flags
+   */
+  visibility?: boolean
 }
 
 /** @internal */
@@ -54,9 +70,23 @@ export const strictMedia = ({ media: options }: StrictPresetOptions = {}): FFFTr
   video: ({ video }) => strictMediaTransform(options, video),
 })
 
+/**
+ * Strict - FFF Transform Preset (Categories/Tags Only)
+ * @alpha
+ */
 export const strictCategories = ({ categories: options }: StrictPresetOptions = {}): FFFTransformPreset => ({
   categories: ({ categories, tags }) => options === true ? [...(categories ?? []), ...(tags ?? [])] : categories,
   tags: ({ categories, tags }) => options === false ? [...(tags ?? []), ...(categories ?? [])] : tags,
+})
+
+export const strictDraft = ({ draft: options }: StrictPresetOptions = {}): FFFTransformPreset => ({
+  draft: ({ draft, flags }) => draft ?? options === true ? flags?.includes('draft') : undefined,
+  flags: ({ draft, flags }) => (options === false && draft) ? [...(flags ?? []), 'draft'] : flags,
+})
+
+export const strictVisibility = ({ visibility: options }: StrictPresetOptions = {}): FFFTransformPreset => ({
+  flags: ({ flags, visibility }) => (options === false && visibility) ? [...(flags ?? []), visibility] : flags,
+  visibility: ({ flags, visibility }) => visibility ?? options === true ? flags?.find(flag => ['private', 'public', 'unlisted'].includes(flag)) : undefined,
 })
 
 /**
@@ -65,5 +95,7 @@ export const strictCategories = ({ categories: options }: StrictPresetOptions = 
  */
 export const strict = (options: StrictPresetOptions): FFFTransformPreset => ({
   ...strictCategories(options),
+  ...strictDraft(options),
   ...strictMedia(options),
+  ...strictVisibility(options),
 })
