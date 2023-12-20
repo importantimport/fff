a848c00f42a1af114924bbd73deb1df5536dbfe2
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-12-20 14:36:26 +0800

    refactor(markdown-it-fff): 🎨 improve structure

---
+++ packages/markdown-it-fff/src/plugin.ts
@@ -0,0 +1,33 @@
+import type MarkdownIt from 'markdown-it'
+
+import { strict, transform } from 'fff-flavored-frontmatter'
+
+import type { FFFPluginOptions, MarkdownItEnv } from './lib/types'
+
+/**
+ * markdown-it plugin for auto-conversion other frontmatter variable formats to {@link https://fff.js.org | FFF Flavored Frontmatter}.
+ * @param md - MarkdownIt
+ * @param options - FFF Flavor Transform Presets & Strict Options
+ * @public
+ */
+export const fffPlugin: MarkdownIt.PluginWithOptions<FFFPluginOptions> = (
+  md,
+  // eslint-disable-next-line unicorn/no-object-as-default-parameter
+  options = { presets: [] },
+) => {
+  const render = md.renderer.render.bind(md.renderer)
+  md.renderer.render = (t, o, env: MarkdownItEnv) => {
+    env.frontmatter = transform(
+      {
+        ...env.frontmatter,
+        excerpt: env.frontmatter?.excerpt ?? env.excerpt,
+        title: env.frontmatter?.title ?? env.title,
+      },
+      [
+        ...options.presets,
+        ...(options.strict ? [strict(options.strict)] : []),
+      ],
+    )
+    return render(t, o, env)
+  }
+}
--- packages/remark-fff/src/index.ts
+++ packages/remark-fff/src/index.ts
@@ -1,3 +1,2 @@
-export type * from './types'
-
 export { remarkFFF, remarkFFF as default } from './plugin'
+export type * from './types'
